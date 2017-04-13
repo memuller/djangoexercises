920d2ce967a5e872d227c5e8ac66d2ecc23fdbe0
Matheus E. Muller <hello@memuller.com>
2017-04-13 18:31:19 -0300

    Adds some basic models.

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Instrument, InstrumentType
+
+admin.site.register(Instrument)
+admin.site.register(InstrumentType)
--- school/models.py
+++ school/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class InstrumentType(models.Model):
+    name =      models.CharField(max_length=50)
+
+class Instrument(models.Model):
+    name =      models.CharField(max_length=200)
+    model =     models.CharField(max_length=100)
+    addedAt =   models.DateField()
+    available = models.BooleanField(default=1)
+
+    kind =      models.ForeignKey(InstrumentType)
